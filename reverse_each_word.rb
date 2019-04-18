d23ec0b30ac1a396b8886f4131b9738aa88bd156
Aniket Patel <aniketupatel1@gmail.com>
2019-04-18 17:17:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   words = string.split("")
   new_array = []
-  words.each do |string|
+  words.each do |word|
     new_array << words.reverse
   end
   new_array.join(" ")
